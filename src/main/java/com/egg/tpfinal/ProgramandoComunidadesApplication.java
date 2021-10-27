28178a52bffc7d5afd602cb213f7888532edb651
nardo96hub <nardelli96@gmail,com>
2021-10-27 20:08:51 -0300

    Proyecto Vacio

---
+++ src/main/java/com/egg/tpfinal/ProgramandoComunidadesApplication.java
@@ -0,0 +1,13 @@
+package com.egg.tpfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramandoComunidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramandoComunidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egg/tpfinal/ProgramandoComunidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.tpfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramandoComunidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

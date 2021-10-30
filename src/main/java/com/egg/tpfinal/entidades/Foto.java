96ed0e9e8f085c8c091ab8f67f31fa3efa1797fc
Tamarapenaranda <91096786+Tamarapenaranda@users.noreply.github.com>
2021-10-30 09:50:26 -0300

    Update Foto.java

--- src/main/java/com/egg/tpfinal/entidades/Foto.java
+++ src/main/java/com/egg/tpfinal/entidades/Foto.java
@@ -1,6 +1,10 @@
 package com.egg.tpfinal.entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity
 public class Foto {

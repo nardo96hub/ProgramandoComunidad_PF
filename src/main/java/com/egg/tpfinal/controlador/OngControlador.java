eec94e9537a74870360e28734bdf58ed3da9ae94
nardo96hub <nardelli96@gmail,com>
2021-11-02 22:23:08 -0300

    Ong Controller funcionando

---
+++ src/main/java/com/egg/tpfinal/controlador/OngControlador.java
@@ -0,0 +1,84 @@
+package com.egg.tpfinal.controlador;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.egg.tpfinal.entidades.ONG;
+import com.egg.tpfinal.entidades.Usuario;
+import com.egg.tpfinal.servicios.OngService;
+import com.egg.tpfinal.servicios.UsuarioService;
+
+import enumeracion.Rol;
+
+@Controller
+@RequestMapping("/ong") 
+public class OngControlador {
+	
+	@Autowired
+	private UsuarioService ServiUsu;
+	
+	@Autowired
+	private OngService ServiOng;
+	
+	
+	@GetMapping("/crearong")
+	public String registro() {
+		return "registroong.html";
+	}
+	@PostMapping("/crearong")
+	public String cargaong(@RequestParam String marca, @RequestParam String name,@RequestParam String ape,@RequestParam String user,@RequestParam String pass)
+	{
+		
+		try {
+		
+			Usuario u =ServiUsu.seteoUsuario(user, pass, Rol.ONG);//Falta validar Usuario
+			
+			
+			ServiOng.crearOng(u, marca, name, ape);
+			System.out.println("Se creo bien");
+			return "redirect:/";
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "redirect:/ong/crearong";
+		}
+		
+	}
+	/*@GetMapping(value="/agregarong")//value=
+	public String mostrarong(Model model) {
+		model.addAttribute("ONG",new ONG());
+		return "registroong.html";
+	}
+	
+	@PostMapping("/crearong")
+	public String cargarong(@ModelAttribute ONG ong ) {
+		ServiOng.saveOng(ong);//En el futuro hacer crearOng para validar
+		return "redirect:/";
+	}
+	*/
+	@GetMapping("/listarong")
+	public String listarong(Model mod) {
+		List<ONG> lo=ServiOng.listarONGactivas();
+		mod.addAttribute("listarOng",lo);
+		//return "listarong";
+		return "redirect:/";
+	}
+	
+	@GetMapping("/eliminarong/{id}")
+	public String eliminarong(@PathVariable Long id) {
+		ServiOng.borrarONG(id);
+		return "redirect:/";
+	}
+	
+	
+
+}
--- src/main/java/com/egg/tpfinal/servicios/OngService.java
+++ src/main/java/com/egg/tpfinal/servicios/OngService.java
@@ -4,8 +4,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.egg.tpfinal.entidades.*;
 import com.egg.tpfinal.repositorios.OngRepository;
+
+import enumeracion.Rol;
 
 @Service
 public class OngService {
@@ -13,7 +18,10 @@ public class OngService {
 
 	@Autowired
 	private OngRepository ONGRepo;
+	@Autowired
+	private UsuarioService ServiUsu;
 	
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
 	public void guardarOng(ONG ong,String marca, String nombre_rep, String apellido_rep,Usuario usuario) {
 		
 		ong.setMarca(marca);
@@ -21,7 +29,10 @@ public class OngService {
 		ong.setApellido_rep(apellido_rep);
 		ong.setUsuario(usuario);
 		ong.setAlta(true);
-				
+		
+		ServiUsu.saveUsuario(usuario);
+		
+		
 		ONGRepo.save(ong);
 				
 	}
@@ -59,6 +70,24 @@ public class OngService {
 	public ONG getONG(Long ID) {
 		Optional<ONG> ong=ONGRepo.findById(ID);
 		return ong.get();
+	}
+	
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
+	public void crearOng(Usuario usuario,String marca,String nombre_rep,String apellido_rep) throws Exception {
+		ONG ong=ONGRepo.marcaOng(marca);//Se puede cambiar por email usuario
+		if(ong==null) {
+			ong=new ONG();
+			guardarOng(ong, marca, nombre_rep, apellido_rep, usuario);
+			
+		}else {
+			throw new Exception("Ya se encuentra la Ong en BDD");
+		}
+	}
+	public void saveOng(ONG ong) {
+		
+		 
+
+		ONGRepo.save(ong);
 	}
 
 	
